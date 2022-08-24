b8bc03c123df9ab41b3afac67f3e95b55daaf68b
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-08-24 10:19:40 +0900

    Create ChartItemValuePercentage file
    #7

---
+++ SSChart/Sources/SSChart/Common/ChartItemValuePercentage.swift
@@ -0,0 +1,14 @@
+//
+//  ChartItemValuePercentage.swift
+//  
+//
+//  Created by YJ on 2022/08/24.
+//
+
+import Foundation
+import UIKit
+
+struct ChartItemValuePercentage {
+    let start: CGFloat
+    let end: CGFloat
+}
