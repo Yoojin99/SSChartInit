cf0f9ea006ec0308277989a8d81262f53d47439f
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-08-25 11:58:41 +0900

    test

--- Package.swift
+++ Package.swift
@@ -21,9 +21,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SSChart",
-            dependencies: []),
+            path: "SSChart/Sources",
+            dependencies: [],
+        ),
         .testTarget(
             name: "SSChartTests",
+            path: "SSChart/Tests",
             dependencies: ["SSChart"]),
     ]
 )
